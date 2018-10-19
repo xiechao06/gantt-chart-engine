d55e0d2cca19e1d5556a87394b467d79e1beb516
xiechao06 <xiechao06@gmail.com>
2018-10-19 17:57:11 +0800

    upgrade rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -4,40 +4,23 @@ import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
 
 export default [
-  // browser-friendly UMD build
-  {
-    entry: 'src/index.js',
-    dest: pkg.browser,
-    format: 'umd',
-    moduleName: 'gantt-engine',
-    plugins: [
-      resolve(),
-      commonjs(),
-      babel({
-        exclude: ['node_modules/**'],
-        externalHelpers: true,
-        plugins: ['@babel/plugin-external-helpers', 'add-module-exports'],
-        presets: ['@babel/preset-env']
-      })
-    ]
-  },
-
   // CommonJS (for Node) and ES module (for bundlers) build.
   // (We could have three entries in the configuration array
   // instead of two, but it's quicker to generate multiple
   // builds from a single configuration where possible, using
   // the `targets` option which can specify `dest` and `format`)
   {
-    entry: 'src/index.js',
+    input: 'src/index.js',
     external: ['timestring'],
-    targets: [
-      { dest: pkg.main, format: 'cjs' },
-      { dest: pkg.module, format: 'es' }
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
     ],
     plugins: [
+      resolve(),
+      commonjs(),
       babel({
         exclude: ['node_modules/**'],
-        plugins: ['@babel/plugin-external-helpers'],
         presets: [['@babel/preset-env', { useBuiltIns: 'usage' }]]
       })
     ]
